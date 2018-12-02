5075cc7112dd2142b217a9c7a8fa2b0d327e0a16
Alan Sanchez <sanchezalan95@gmail.com>
2018-12-01 18:33:13 -0600

    Configuracion de la base de datos

--- config/Database.php
+++ config/Database.php
@@ -2,16 +2,18 @@
 
     class Database{
         //Credentials
-        private $host = 'localhost';
-        private $dbName = 'feeds';
+        private $host = '127.0.0.1';
+        private $dbName = 'rss';
         private $username = 'root';
         private $password = '';
+        private $charset = 'utf8mb4';
         public $conn;
 
         public function getConnection(){
             $this->conn = null;
             try {
-                $this->conn = new PDO("mysql:host=". $this->host . "dbname=".$this->dbName,$this->username, $this->password);
+                $dsn = "mysql:host={$this->host};dbname={$this->dbName};charset={$this->charset}";
+                $this->conn = new PDO($dsn, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
             } catch (PDOException $exception) {
                 echo "Connection error: ". $exception->getMessage();
