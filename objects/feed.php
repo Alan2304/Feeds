8f750e80b4207787f22d998bcebf9fb654c81558
Alan Sanchez <sanchezalan95@gmail.com>
2018-12-02 14:36:12 -0600

    Se creo el objeto feed

--- objects/feed.php
+++ objects/feed.php
@@ -15,17 +15,29 @@ class Feed{
         $this->conn = $db;
     }
 
+    function all(){
+        $query = "SELECT title, date, description, permalink FROM feeds";
+        $stmt = $this->conn->prepare($query);  
+        $stmt->execute();
+        return $stmt;
+    }
+
     function search($keyword){
         //Match Query
-
         $query = "SELECT title, date, MATCH(title,description) AGAINST( '{$keyword}' ) AS SCORE
-                        FROM feeds WHERE MATCH(title,description) AGAINST ( '{$keyword}' ) ORDER BY SCORE DESC";
+                        FROM feeds WHERE MATCH(title,description) AGAINST ( '{$keyword}' ) ORDER BY SCORE DESC LIMIT 10";
         $stmt = $this->conn->prepare($query);    
         //bind
         $stmt->bindValue(1, $keyword, PDO::PARAM_STR);
         $stmt->bindValue(2, $keyword, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt;
+    }
+
+    function insert(){
+        $query = "INSERT INTO feeds (title,description,date,permalink) VALUES('{$this->title}', '{$this->description}', '{$this->date}', '{$this->permalink}')";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
     }
 
 }
