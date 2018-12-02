24d46c6a48e7fb44eb7ee5b28cc1f74b8c98804b
Alan Sanchez <sanchezalan95@gmail.com>
2018-12-02 14:36:41 -0600

    Ahora el archivo feeds.php regsitra todos los feeds en la base de datos

--- feeds.php
+++ feeds.php
@@ -1,44 +1,41 @@
 <?php
-
+  // required headers
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  
   require_once 'vendor/autoload.php';
+  // include database and object files
+  include_once './config/database.php';
+  include_once './objects/feed.php';
 
   $url = 'http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml';
   $feed = new SimplePie();
   $feed->set_feed_url($url);
   $feed->enable_cache();
   $feed->init();
-?>
 
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-     <link rel="stylesheet" href="css/main.css">
-     <title>RSS</title>
-   </head>
-   <body>
-     <?php
-      echo '<h1>' . $feed->get_title() . '</h1>';
-      echo '<p>' . $feed->get_description() . '</p>';
-      ?>
-      <div class="container">
-        <div class="row">
-          <?php foreach ($feed->get_items() as $item): ?>
-            <div class="col-md-4 my-3">
-              <div class="card feed">
-                <div class="card-header">
-                    <?php echo $item->get_title(); ?>
-                </div>
-                <div class="card-body">
-                  <p><?php echo $item->get_description(); ?></p>
-      			      <p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
-                  <a href="<?php echo $item->get_permalink(); ?>">Ver Noticia Completa</a>
-                </div>
-              </div>
-            </div>
-          <?php endforeach; ?>
-        </div>
-      </div>
-</body>
-</html>
+  // instantiate database and product object
+  $database = new Database();
+  $db = $database->getConnection();
+
+  //Initialize object
+  $feedObj = new Feed($db);
+
+  foreach ($feed->get_items() as $item):
+    $feedObj->title = $item->get_title();
+    $feedObj->description = $item->get_description();
+    $feedObj->date = $item->get_date('j F Y | g:i a');
+    $feedObj->permalink = $item->get_permalink();
+    $feedObj->insert();
+  endforeach;
+
+  $response = array(
+    'response' => 'OK'
+  );
+
+  //set reponse code 200 PK
+  http_response_code(200);
+
+  //Show products data
+  echo json_encode($response);
+?>
