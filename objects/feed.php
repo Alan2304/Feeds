b1711d1fa98c90a6f0295b296536daa8d9b94789
Alan Sanchez <sanchezalan95@gmail.com>
2018-12-03 11:28:31 -0600

    Se cambió la función insert debido a un warning al ejecutar el query

--- objects/feed.php
+++ objects/feed.php
@@ -27,17 +27,14 @@ class Feed{
         $query = "SELECT title, date, MATCH(title,description) AGAINST( '{$keyword}' ) AS SCORE
                         FROM feeds WHERE MATCH(title,description) AGAINST ( '{$keyword}' ) ORDER BY SCORE DESC LIMIT 10";
         $stmt = $this->conn->prepare($query);    
-        //bind
-        $stmt->bindValue(1, $keyword, PDO::PARAM_STR);
-        $stmt->bindValue(2, $keyword, PDO::PARAM_STR);
         $stmt->execute();
         return $stmt;
     }
 
     function insert(){
         $query = "INSERT INTO feeds (title,description,date,permalink) VALUES('{$this->title}', '{$this->description}', '{$this->date}', '{$this->permalink}')";
-        $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+        //$stmt = $this->conn->prepare($query);
+        $this->conn->exec($query);
     }
 
 }
